cad2329583786ee9e88754c22038bbec185dc9d4
leonardohernandesq <leonardo.agencia7@c7business.com>
2022-09-17 02:54:33 +0100

    versionamento de codigo - banner

--- src/Components/Banner/style.js
+++ src/Components/Banner/style.js
@@ -2,8 +2,24 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     width: 100%;
-
     img{
         width: 100%;
+        z-index: 9;
+    }
+    >.banner{
+        position: relative;
+    }
+    .banner h1{
+        font-size: 54px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: transparent;
+        position: absolute;
+        bottom: 0;
+        top: 0;
+        right: 0;
+        left: 0;
+        z-index: 3;
     }
 `;
