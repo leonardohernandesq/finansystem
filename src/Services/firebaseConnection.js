20872b9171d09aef34903ace2f53a5f0d91cd0d1
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2022-09-21 01:36:33 +0100

    Update firebaseConnection.js

--- src/Services/firebaseConnection.js
+++ src/Services/firebaseConnection.js
@@ -6,13 +6,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxjeIWIsalPPdFsKublM-aN_-XgIN6anU",
-    authDomain: "finansystem-e5034.firebaseapp.com",
-    projectId: "finansystem-e5034",
-    storageBucket: "finansystem-e5034.appspot.com",
-    messagingSenderId: "131661957709",
-    appId: "1:131661957709:web:c6396897c934a8ea76aa04",
-    measurementId: "G-X2JKKT0C8C"
+    apiKey: "**************************************",
+    authDomain: "**************************************",
+    projectId: "**************************************",
+    storageBucket: "**************************************",
+    messagingSenderId: "**************************************",
+    appId: "**************************************",
+    measurementId: "**************************************"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
