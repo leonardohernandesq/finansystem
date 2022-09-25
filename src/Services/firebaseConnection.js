d447b08c31c5acea7052721882ace2866c887ea0
Leonardo Hernandes <leonardo_hernandes@outlook.com.br>
2022-09-25 03:45:40 +0100

    Revert "Revert "Refatoração de codigo em algumas páginas e criação de algumas funcionalidades do firebase""
    This reverts commit 2299e673f1eb030ffc138012530a4d81ac569ec3.

--- src/Services/firebaseConnection.js
+++ src/Services/firebaseConnection.js
@@ -1,21 +1,20 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxjeIWIsalPPdFsKublM-aN_-XgIN6anU",
-    authDomain: "finansystem-e5034.firebaseapp.com",
-    projectId: "finansystem-e5034",
-    storageBucket: "finansystem-e5034.appspot.com",
-    messagingSenderId: "131661957709",
-    appId: "1:131661957709:web:c6396897c934a8ea76aa04",
-    measurementId: "G-X2JKKT0C8C"
+    apiKey: "**************************************",
+    authDomain: "**************************************",
+    projectId: "**************************************",
+    storageBucket: "**************************************",
+    messagingSenderId: "**************************************",
+    appId: "**************************************",
+    measurementId: "**************************************"
 };
 
-const firebase = initializeApp(firebaseConfig);
-const auth = getAuth(firebase);
-const db = getFirestore(firebase);
-const storage = getStorage(firebase)
+const app = initializeApp(firebaseConfig);
 
-export { db, auth, storage }
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth }
